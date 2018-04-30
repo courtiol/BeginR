7afe08c9a985661b60f42fedf95bcef5ea18606d
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-04-30 16:33:49 +0200

    first commit

---
+++ R/BeginR-package.R
@@ -0,0 +1,11 @@
+#' This is the R package...
+#'
+#' A small description here...
+#'
+#' @name BeginR-package
+#' @aliases BeginR-package BeginR
+#' @docType package
+#'
+#' @keywords package
+#'
+NULL
