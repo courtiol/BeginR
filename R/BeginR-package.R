cb64f6f0c789738b808eb0c399ce1c8ea25b6c57
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-06-05 11:42:49 +0200

    add general help description

--- R/BeginR-package.R
+++ R/BeginR-package.R
@@ -1,11 +1,16 @@
-#' This is the R package...
+#' This is the R package Getting Started with R
 #'
-#' A small description here...
+#' The goal of this package is to provide basic information about how to use R.
+#' This is part of a course designed and taught by Alexandre Courtiol, IZW, Berlin.
+#' Liam Bailey and Colin Vullioud also contributed to create some slides (provided as vignettes).
+#'
+#' The core of this package is its collection of vignettes, see examples.
 #'
 #' @name BeginR-package
 #' @aliases BeginR-package BeginR
 #' @docType package
 #'
 #' @keywords package
-#'
+#' @examples
+#' browseVignettes(package = 'BeginR')
 NULL
