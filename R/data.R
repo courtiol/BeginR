c5076a27beef745f0c8efdbfae03d1c96ed77847
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-06-12 18:42:06 +0200

    add UK population and mortality datasets

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' The population census for England and Wales (1901-2016)
+#'
+#' This dataset contains the total population count (per age classes) for England and Wales over the period 1901-2016.
+#'
+#' @name population_UK
+#' @docType data
+#' @format A \var{dataframe} with 1392 obs. of 4 variables.
+#' @source These data were derived files freely available from the Office for National Statistics (\url{http://webarchive.nationalarchives.gov.uk}).
+#' @keywords datasets
+#' @note These data have been prepared for use in the paper "The transition to modernity and chronic disease: mismatch and natural selection" by Stephen Corbett, Alexandre Courtiol, Virpi Lummaa, Jacob Moorad & Stephen Stearns published in Nature Reviews Genetics (2018): \url{https://www.nature.com/articles/s41576-018-0012-3}.
+#' @examples
+#' head(population_UK)
+NULL
+
+
+#' The deaths census for England and Wales (1901-2016)
+#'
+#' This dataset contains numbers and causes of death for England and Wales over the period 1901-2016.
+#' The causes of deaths have been classified using the International Classification of Diseases (ICD).
+#'
+#' @name deaths_UK
+#' @docType data
+#' @format A \var{dataframe} 1445659 obs. of 8 variables.
+#' @source These data were derived files freely available from the Office for National Statistics (\url{http://webarchive.nationalarchives.gov.uk}).
+#' @keywords datasets
+#' @note These data have been prepared for use in the paper "The transition to modernity and chronic disease: mismatch and natural selection" by Stephen Corbett, Alexandre Courtiol, Virpi Lummaa, Jacob Moorad & Stephen Stearns published in Nature Reviews Genetics (2018): \url{https://www.nature.com/articles/s41576-018-0012-3}.
+#' @examples
+#' head(deaths_UK)
+NULL
