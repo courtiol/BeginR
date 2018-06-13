0e9023f149b826c3b8ff352bdaca804577605672
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-06-13 11:08:18 +0200

    new welcome message with github link and get_vignettes

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,12 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    "\n ##########################################",
-    "\n #                                        #",
-    "\n #   This is the package for the course   #",
-    "\n #                                        #",
-    "\n #        'Getting Started with R'        #",
-    "\n #                                        #",
-    "\n #      Version ", utils::packageDescription("BeginR")$Version," installed      #",
-    "\n #                                        #",
-    "\n #     To access the slides, just type    #",
-    "\n #                                        #",
-    "\n #   browseVignettes(package = 'BeginR')  #",
-    "\n #                                        #",
-    "\n ##########################################", "\n")
+    "\n The package for the course 'Getting Started with R'",
+    "\n by @alexcourtiol (version ", utils::packageDescription("BeginR")$Version, "), is now loaded!",
+    "\n To access the slides, just type browseVignettes(package = 'BeginR'),",
+    "\n [or get_vignettes() if you also need to see the sources of the vignettes].",
+    "\n",
+    "\n All sources for this package are available at https://github.com/courtiol/BeginR",
+    "\n where you can find more information on how to use this package",
+    "\n and where you can also leave comments (under 'Issues').",
+    "\n")
 }
