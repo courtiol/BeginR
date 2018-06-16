8cf21f46a027b7f4dc9cf140773032c5f57c69ef
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-06-16 14:44:00 +0200

    fix data documentation

--- R/data.R
+++ R/data.R
@@ -2,7 +2,6 @@
 #'
 #' This dataset contains the total population count (per age classes) for England and Wales over the period 1901-2016.
 #'
-#' @name population_UK
 #' @docType data
 #' @format A \var{dataframe} with 1392 obs. of 4 variables.
 #' @source These data were derived files freely available from the Office for National Statistics (\url{http://webarchive.nationalarchives.gov.uk}).
@@ -10,15 +9,13 @@
 #' @note These data have been prepared for use in the paper "The transition to modernity and chronic disease: mismatch and natural selection" by Stephen Corbett, Alexandre Courtiol, Virpi Lummaa, Jacob Moorad & Stephen Stearns published in Nature Reviews Genetics (2018): \url{https://www.nature.com/articles/s41576-018-0012-3}.
 #' @examples
 #' head(population_UK)
-NULL
-
+"population_UK"
 
 #' The deaths census for England and Wales (1901-2016)
 #'
 #' This dataset contains numbers and causes of death for England and Wales over the period 1901-2016.
 #' The causes of deaths have been classified using the International Classification of Diseases (ICD).
 #'
-#' @name deaths_UK
 #' @docType data
 #' @format A \var{dataframe} 1445659 obs. of 8 variables.
 #' @source These data were derived files freely available from the Office for National Statistics (\url{http://webarchive.nationalarchives.gov.uk}).
@@ -26,7 +23,7 @@ NULL
 #' @note These data have been prepared for use in the paper "The transition to modernity and chronic disease: mismatch and natural selection" by Stephen Corbett, Alexandre Courtiol, Virpi Lummaa, Jacob Moorad & Stephen Stearns published in Nature Reviews Genetics (2018): \url{https://www.nature.com/articles/s41576-018-0012-3}.
 #' @examples
 #' head(deaths_UK)
-NULL
+"deaths_UK"
 
 #' Sample from the Ten-Year Follow-up (1980) of the 1970 British Cohort Study
 #'
@@ -80,8 +77,7 @@ NULL
 #'   \item{father_weight}{Father's weight in Kg}
 #' }
 #' @source Study number 3723 from the BCS70: \url{https://discover.ukdataservice.ac.uk/series/?sn=200001}
-NULL
-
+"UK"
 
 #' Sample from the Health Survey for England Cardiovascular Disease’98
 #'
@@ -100,7 +96,7 @@ NULL
 #' }
 #'
 #' @source UK Data Archive Study Number 4150
-NULL
+"HSE98women"
 
 #' The Gryphon dataset
 #'
@@ -122,7 +118,7 @@ NULL
 #'    Morrissey, Erik Postma, Craig A. Walling, Loeske E. B. Kruuk; Daniel H.
 #'    Nussey; An ecologist's guide to the animal model. Journal of Animal
 #'    Ecology 2010; 79, 13--26. doi: 10.1111/j.1365-2656.2009.01639.x
-NULL
+"Gryphon"
 
 #' The Surprise dataset
 #'
@@ -140,8 +136,7 @@ NULL
 #'   }
 #'
 #' @source Justin Matejka, George Fitzmaurice, Toronto Ontario Canada. DOI: http://dx.doi.org/10.1145/3025453.3025912
-NULL
-
+"Surprise"
 
 #' The Challenger Space Shuttle O-Ring Dataset
 #'
@@ -174,4 +169,4 @@ NULL
 #' @source \url{https://archive.ics.uci.edu/ml/datasets/Challenger+USA+Space+Shuttle+O-Ring}
 #'
 #' Original dataset from: Draper,D. (1993). Assessment and propagation of model uncertainty. In Proceedings of the Fourth International Workshop on Artificial Intelligence and Statistics (pp. 497--509). Ft. Lauderdale, FL: Unpublished.
-NULL
+"Challenger"
